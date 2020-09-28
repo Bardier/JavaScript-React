f7e6b716fb693bc451294ca0d203cc2da8566b1e
Byefar <bardier.pro@gmail.com>
2020-09-28 19:00:11 +0300

    Первый комит

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// alert ("privet");
+// let answer = prompt('Тут задаем вопрос', 'значение по умолчанию');
+// console.log( typeof(answer) );
+
+const answers = [];
+answers[99] = 1;
+console.log(answers.length);
+
+let incr = 10,
+    decr = 10;
+ 
+console.log(incr++, decr--);
