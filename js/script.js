ce03e6b42bf4bdd6f6e13e616d079519f60bba7f
Byefar <bardier.pro@gmail.com>
2020-10-04 14:13:15 +0300

    start 04.10.2020

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 // let answer = prompt('Тут задаем вопрос', 'значение по умолчанию');
 // console.log( typeof(answer) );
 
-const answers = [];
-answers[99] = 1;
-console.log(answers.length);
+// const answers = [];
+// answers[99] = 1;
+// console.log(answers.length);
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
  
-console.log(incr++, decr--);
+// console.log(incr++, decr--);
+
+const numberOfFilms = 0;
