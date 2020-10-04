1771f8991a4506de8da1972783257f032e195584
Byefar <bardier.pro@gmail.com>
2020-10-04 14:45:45 +0300

    Первая практика, создание обьектаи добавление ему свойств

--- js/script.js
+++ js/script.js
@@ -11,4 +11,23 @@
  
 // console.log(incr++, decr--);
 
-const numberOfFilms = 0;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
+
+
+const personalMovieDB = {
+    coutn: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько вы его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько вы его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB.movies);
